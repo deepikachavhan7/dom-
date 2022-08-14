3352b44ae8fe1c5b63deef0defec7c065bf5cb47
deepikachavhan7 <deepikachavhan7@gmail.com>
2022-08-14 10:18:03 +0530

    delete data in main4.js

--- main3.js
+++ main3.js
@@ -33,3 +33,7 @@ function onclickdata(e){
     
 }
 
+
+
+
+
--- main4.js
+++ main4.js
@@ -13,20 +13,59 @@ function onsubmitdata(event){
     const username=event.target.uname.value;
     const usermail=event.target.uemail.value;
     const userpass=event.target.upassword.value;
-    const newuser1={
+    const newuser2={
         username,
         usermail,
         userpass
     }
-    var storedata= localStorage.setItem("user1.usermail",newuser1);
-    // console.log(storedata)
-   showdatainli(newuser1)
-   
+    localStorage.setItem(newuser2.usermail,JSON.stringify( newuser2));
+    showUserOnScreen(newuser2)
 }
+/// iterate over object key 
+window.addEventListener("DOMContentLoaded",()=>{
+    const localStorageObj = localStorage;
+    const localStoragekeys= Object.keys(localStorageObj)
 
-function showdatainli(user){
+    for (var i=0 ; i< localStoragekeys.length;i++){
+        const key =localStoragekeys[i]
+        const userDetailsString =localStorageObj[key];
+        const useerDetailsObj = JSON.parse(userDetailsString);
+        showUserOnScreen(useerDetailsObj);
+    }
+})
+
+ 
+
+
+// function to show data on screen
+function showUserOnScreen(user){
+     
+
+
+
     const parentnode =document.getElementById("users");
-    const childhtml= `<li>${user.username} :${user.usermail}:${user.userpass}</li>`;
+    const childhtml= `<li>${user.username} :${user.usermail}:${user.userpass}
+    <button onClick= deleteUser('${user.usermail}')> Delete User </button>
+   
+    </li>`;
     parentnode.innerHTML=parentnode.innerHTML + childhtml;
 }
 
+
+
+
+// delelte user
+
+function deleteUser(uemail){
+    console.log(uemail)
+    localStorage.removeItem(uemail)
+    removeUserFromScreen(uemail)
+}
+
+function removeUserFromScreen(uemail){
+    const parentNode = document.getElementById('users');
+    const childnodetodeleted =document.getElementById(uemail);
+    
+    parentNode.removeChild(childnodetodeleted)
+
+}
